d83a03d769d8d2940524a6c071ccc05130db5f93
zn <244305150@qq.com>
2022-01-11 14:02:35 +0800

    修改单耗单位

--- db/migrate/20220111020957_update_stc_to_day_rpt_stc.rb
+++
@@ -1,8 +0,0 @@
-class UpdateStcToDayRptStc < ActiveRecord::Migration
-  def change
-    change_column :day_rpt_stcs, :tp_cost, :decimal, :precision => 10, :scale => 4,  null: false, :default => Setting.systems.default_num
-    change_column :day_rpt_stcs, :tn_cost, :decimal, :precision => 10, :scale => 4,  null: false, :default => Setting.systems.default_num
-    change_column :day_rpt_stcs, :tp_utcost, :decimal, :precision => 10, :scale => 4,  null: false, :default => Setting.systems.default_num
-    change_column :day_rpt_stcs, :tn_utcost, :decimal, :precision => 10, :scale => 4,  null: false, :default => Setting.systems.default_num
-  end
-end
